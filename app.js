ee0fe826c104b7bdc7346009d927e1a5c3cd532f
Alexis LAURADOUX <alexis.lauradoux@ynov.com>
2019-05-24 21:32:17 +0200

    port

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ var nunjucks = require('nunjucks');
 
 var app = express();
 
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/mk', express.static(__dirname + '/node_modules/leaflet.markercluster/dist'));
+app.use('/ls', express.static(__dirname + '/node_modules/leaflet-search'));
 
-app.use('/css', express.static( __dirname + '/node_modules/bootstrap/dist/css'));
-app.use('/mk', express.static( __dirname + '/node_modules/leaflet.markercluster/dist'));
-app.use('/ls', express.static( __dirname + '/node_modules/leaflet-search'));
-
-
-app.use('/customcss', express.static( __dirname + '/src/style'));
-app.use('/script', express.static( __dirname + '/src/scripts'));
-
+app.use('/customcss', express.static(__dirname + '/src/style'));
+app.use('/script', express.static(__dirname + '/src/scripts'));
 
 app.use('/', require('./routes/roads'));
 
@@ -21,6 +18,5 @@ nunjucks.configure('views', {
     express: app
 });
 
-
 console.log('Test serveur lancee sur le serveur port 3000');
-app.listen(3000);
+app.listen(process.env.PORT || 5000);
