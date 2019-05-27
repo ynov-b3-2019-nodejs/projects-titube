d6612be88e4997a6f9a770d13c1513fef36d531f
Alexis LAURADOUX <alexis.lauradoux@ynov.com>
2019-05-27 12:07:16 +0200

    Try

--- app.js
+++ app.js
@@ -10,29 +10,61 @@ const app = express();
 const passport = require('passport')
     , LocalStrategy = require('passport-local').Strategy;
 
+const session = require("express-session"),
+    bodyParser = require("body-parser");
+
 let User = [{ username: 'username', password: 'password' }, { username: 'username2', password: 'password' }];
 
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const flash = require('connect-flash');
+app.use(flash());
 
-// Use the GoogleStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Google
-//   profile), and invoke a callback with a user object.
+app.use(express.cookieParser());
+app.use(express.session({ secret: "secret" }));
+
+//app.use(passport.initialize());
+//app.use(passport.session()); // persistent login sessions
+
+app.use(flash());
+app.use(app.router);
+app.use(express.static('public'));
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static("public"));
+app.use(session({ secret: "cats" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "https://accounts.google.com/o/oauth2/auth"
 },
-    function (accessToken, refreshToken, profile, done) {
-        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ googleId: profile.id }, function(err, user) {
             return done(err, user);
         });
     }
 ));
 
 passport.use(new LocalStrategy(
-    function (username, password, done) {
-        User.find({ username: username }, function (err, user) {
+    function(username, password, done) {
+        User.find({ username: username }, function(err, user) {
             if (err) { return done(err); }
             if (!user) {
                 return done(null, false, { message: 'Incorrect username.' });
